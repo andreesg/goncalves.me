1ebfe4bfb8e8c801acfd6b497ac3ffcb878d6a6f
André Gonçalves <andreslb1@gmail.com>
2014-05-06 00:46:54 +0100

    js major update

--- js/main.js
+++ js/main.js
@@ -1,74 +1,79 @@
-/* 
-  Define functions for mouse over styles on links 
-*/
-mouseover = function(link, type) {
-  var text = link.innerHTML;
-  if (type) {
-    link.innerHTML = "<font color='#0077e9'>></font>&nbsp;" + text;
-  } else {
-    link.innerHTML = "<font color='#0077e9'>/</font>&nbsp;" + text;
+var app = {
+  description: {
+    '1': '/ entrepreneur',
+    '2': '/ hairy',
+    '3': '/ thinker',
+    '4': '/ fish allergic',
+    'total': 4
+  },
+  timer: null,
+  counter: 2,
+  image: $("#img-src"),
+
+  init: function() {
+    app.triggerEvents();
+  },
+
+  showImage: function() {
+    console.log("show image");
+    /* Change image src and update image description */
+    $("#img-src").attr('src', 'img/' + (app.counter + 1) + ".png").ready(function() {
+      $("#img-description").html(app.description['' + (app.counter + 1)]);
+    });
+
+    /* keep looping */
+    if (app.counter + 1 == app.description.total) {
+      app.counter = 0;
+    } else {
+      app.counter++;
+    }
+  },
+
+  imageAction: function() {
+    console.log("imageaction");
+    /* function to show image at 1000ms */
+    app.timer = setInterval(app.showImage, 1000);  
+  },
+
+  triggerEvents: function() {
+    var self = this;
+    $(".personal-url").hover(function() {
+      var text = $(this).html();
+      $(this).html("<font color='#0077e9'>/</font>&nbsp;" + text);
+    }, function() {
+      $(this).html($(this).attr("rel"));
+    });
+
+    $(".selected-work").hover(function() {
+      var text = $(this).html();
+      $(this).html("<font color='#0077e9'>></font>&nbsp;" + text);
+    }, function() {
+      $(this).html($(this).attr("rel"));
+    });
+
+    /* Author title hover to show details */
+    $("#author-title").hover(function() {
+      $("#name_details").fadeIn();
+    }, function() {
+      $("#name_details").fadeOut();
+    });
+
+    this.image.hover(self.imageAction, function() {
+      /* mouse out of image */
+      console.log("mouseout");
+      if (app.timer) {
+        clearInterval(app.timer);
+        app.timer = null;
+      }
+      $("#img-description").html('');
+    });
+
   }
 }
-
-mouseout = function(link) {
-  link.innerHTML = $(link).attr("rel");
-}
-
-/* 
-  Global var's 
-*/
-/* Dictionary to get images descriptions */
-var description = {
-  '1': '/ entrepreneur',
-  '2': '/ hairy',
-  '3': '/ thinker',
-  '4': '/ fish allergic',
-  'total': 4
-}
-
-var timer = null, counter = 2;
-var total = description.total;
-
 
 /* 
   ON DOM ready 
 */
 $(document).ready(function() {
-  /* Author title hover to show details */
-  $("#author-title").hover(function() {
-    $("#name_details").fadeIn();
-  }, function() {
-    $("#name_details").fadeOut();
-  });
-
-  /* Change image on hover */
-  $('#img-src').hover(function() {
-    var $img = $(this);
-    $(function() {
-      timer = setInterval(showimage, 1000);
-
-      /* function to show image at 1000ms */
-      function showimage() {
-
-        /* Change image src and update image description */
-        $img.attr('src', 'img/' + (counter+1) + ".png").ready(function() {
-          $("#img-description").html(description['' + (counter+1)]);
-        });
-
-        /* keep looping */
-        if (counter+1 == total) {
-          counter = 0;
-        } else {
-          counter++;
-        }
-      }
-    });
-  }, function() {
-    /* mouse out of image */
-    if (timer) {
-      clearInterval(timer);
-      timer = null;
-    }
-    $("#img-description").html('');
-  });
+  app.init();
 });
